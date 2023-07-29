98d686646909c74ddb88672ef6c22ec24e125b88
habibur-01 <h.r.habibur08@gmail.com>
2023-07-29 15:20:58 +0600

    first commit

---
+++ avg.js
@@ -0,0 +1,21 @@
+var a=119;
+var remainder= a%5;
+console.log(remainder);
+var m=80;
+var e=75;
+var b=79;
+var c=90;
+var p= 85;
+var i=95;
+var h=82;
+var total= m+e+b+c+p+i+h;
+var avg= total/7;
+avg= avg.toFixed(2);
+console.log(avg);
+console.log(parseInt(avg));
+var m="my little"
+var n="playground"
+var field= m + n;
+console.log(field);
+var field= m + ' ' + n;
+console.log(field);
---
+++ firstjs.js
@@ -0,0 +1,7 @@
+var a=1000;
+var b=550;
+var c=300;
+var total= b+c;
+var iHave= a-total;
+
+console.log(iHave);
